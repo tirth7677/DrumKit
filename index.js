ab486262ca8a59e89f08f254bde0c215666882da
Tirth <65870526+tirth7677@users.noreply.github.com>
2023-01-19 00:54:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let numberofdrum = document.querySelectorAll(".drum");
+for (let i = 0; i < numberofdrum.length; i++) {
+  numberofdrum[i].addEventListener("click", function () {
+    var buttoninnerHTML = this.innerHTML;
+    makescound(buttoninnerHTML)
+  });
+}
+document.addEventListener("keypress", function (event) {
+  makescound(event.key)
+});
+function makescound(keyword){
+    switch (keyword) {
+        case "w":
+          let tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.play();
+          break;
+        case "a":
+          let tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.play();
+          break;
+        case "s":
+          let tom3 = new Audio("sounds/tom-3.mp3");
+          tom3.play();
+          break;
+        case "d":
+          let tom4 = new Audio("sounds/tom-4.mp3");
+          tom4.play();
+          break;
+        case "j":
+          let snare = new Audio("sounds/snare.mp3");
+          snare.play();
+          break;
+        case "k":
+          let crash = new Audio("sounds/crash.mp3");
+          crash.play();
+          break;
+        case "l":
+          let kick = new Audio("sounds/kick-bass.mp3");
+          kick.play();
+          break;
+      }
+}
